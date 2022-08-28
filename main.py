9d6f66ef8636e309faa5f5fe8a05c5b17af196cb
Nicolás Hussein <80422357+nhussein11@users.noreply.github.com>
2022-08-28 09:20:28 -0300

    apply: codestyle

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
@@ -10,18 +9,14 @@ from selenium.webdriver.common.by import By
 import time
 from random import randint
 
-
 def random_html_elements_selection(html_elements: list) -> int:
     return randint(1, len(html_elements)-1)
 
-
 def fill_form():
     sections = driver.find_elements(By.CLASS_NAME, "Qr7Oae")
-
     for section in sections[:-2]:
         radio_buttons = section.find_elements(By.CLASS_NAME, "Od2TWd")
         random_selection = random_html_elements_selection(radio_buttons)
-
         driver.execute_script("arguments[0].click();", radio_buttons[random_selection])
         time.sleep(0.25)
 
@@ -34,12 +29,9 @@ def fill_form():
 
 #Enter your URL here
 URL = "(here)"
-
 options = Options()
-
 #Uncomment this line if you want to run the script without opening a new window
 # options.add_argument('--headless')
-
 driver = webdriver.Firefox(service=Service(
     GeckoDriverManager().install()), options=options)
 
